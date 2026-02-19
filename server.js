4094bf563b3844f4c6412cf866e3c16da138f49a
Ubuntu <rokadediksha>
2026-02-19 10:56:11 +0000

    updated code

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
-const express = require("express");
+const http = require("http");
+const fs = require("fs");
 const path = require("path");
 
-const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+const server = http.createServer((req, res) => {
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    if (req.url === "/" || req.url === "/index.html") {
+
+        const filePath = path.join(__dirname, "index.html");
+
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(500);
+                res.end("Error loading file");
+            } else {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(data);
+            }
+        });
+
+    } else {
+        res.writeHead(404);
+        res.end("Page Not Found");
+    }
+
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
 
